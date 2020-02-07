b24f7de6bc06e8eb29f98398d887de3a52ffa09d
GabovYegor <gabov.egor2017@yandex.ru>
2020-02-07 12:30:02 +0300

    new tests

---
+++ shaderToDifFiles/build/CMakeFiles/lib.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/egor/Documents/OpenGL/shaderToDifFiles/Shader/Shader.cpp" "/home/egor/Documents/OpenGL/shaderToDifFiles/build/CMakeFiles/lib.dir/Shader/Shader.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../dir"
+  "../PRIVATE"
+  "../Shader"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
