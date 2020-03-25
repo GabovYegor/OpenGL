1be80a222caa9d4cba5c0df3adf2fb96faf05889
Egor Gabov <Egor.Gabov@waveaccess.ru>
2020-03-25 19:11:42 +0300

    test

--- CC_Lab3/glwidget.h
+++ CC_Lab3/glwidget.h
@@ -21,7 +21,7 @@ private:
     void drawLogic(const Triangle &triangle, int reqursionDeep, int test);
     int reqursionDeep;
     int reqursionVecHelper;
-    int iterationNumber = 3;
+    int iterationNumber = 2;
     std::vector <QPointF> reqursionVec;
     std::vector <QPointF> tempVec;
 };
